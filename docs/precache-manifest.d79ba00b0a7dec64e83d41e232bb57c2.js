4564ef74d549f227d40cb523eba40f070eb54ead
gxz <ganxunzou@163.com>
2020-11-30 17:17:09 +0800

    增加github.io示例

---
+++ docs/precache-manifest.d79ba00b0a7dec64e83d41e232bb57c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2da4b390aa1e87f63935b279e84e3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ad5b030a9f4e1dbbe7",
+    "url": "/static/css/main.3489b6fd.chunk.css"
+  },
+  {
+    "revision": "624fbd5344cd4c68d6bb",
+    "url": "/static/js/2.f9b341b4.chunk.js"
+  },
+  {
+    "revision": "ed3e0a901e8984ecf6c802ccfdf37a6e",
+    "url": "/static/js/2.f9b341b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ad5b030a9f4e1dbbe7",
+    "url": "/static/js/main.d3ba8b61.chunk.js"
+  },
+  {
+    "revision": "712a493cb6bbaa95b925",
+    "url": "/static/js/runtime-main.cef06927.js"
+  }
+]);
